77a869ae29c0bc61a8096ed301c82b6b184e70b2
jmkim <wjdals0304@gmail.com>
2022-03-23 11:29:14 +0900

    add:  search bar 검색시  로직 추가

--- BookReviewApp/Scene/SearchBook/SearchBookPresenter.swift
+++ BookReviewApp/Scene/SearchBook/SearchBookPresenter.swift
@@ -11,31 +11,50 @@ protocol SearchBookProtocol {
     
     func setupViews()
     func dismiss()
-    
+    func reloadView()
+}
+
+protocol SearchBookDelegate {
+    func selectBook(_ book: Book)
 }
 
 final class SearchBookPresenter: NSObject {
     
     private let viewController: SearchBookProtocol
+    private let bookSearchManager = BookSearchManager()
     
-    init(viewController: SearchBookProtocol) {
+    private let delegate : SearchBookDelegate
+    
+    private var books: [Book] = []
+    
+    init(viewController: SearchBookProtocol, delegate: SearchBookDelegate ) {
         self.viewController = viewController
+        self.delegate = delegate
     }
     
     func viewDidLoad() {
         viewController.setupViews()
     }
-    
 }
 
 extension SearchBookPresenter: UISearchBarDelegate {
     
-    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchText = searchBar.text else { return }
+        
+        bookSearchManager.request(from: searchText) { [weak self] newBooks in
+            self?.books = newBooks
+            self?.viewController.reloadView()
+        }
+    }
 }
 
 extension SearchBookPresenter: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedBook = books[indexPath.row]
+        delegate.selectBook(selectedBook)
+        
         viewController.dismiss()
     }
     
@@ -44,12 +63,12 @@ extension SearchBookPresenter: UITableViewDelegate {
 extension SearchBookPresenter: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        3
+        books.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "\(indexPath)"
+        cell.textLabel?.text = books[indexPath.row].title
         return cell
     }
     
