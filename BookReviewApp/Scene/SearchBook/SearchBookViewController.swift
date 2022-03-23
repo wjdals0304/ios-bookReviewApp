05326b842e1adae2dc94c9aefd11a0b47785bb15
jmkim <wjdals0304@gmail.com>
2022-03-23 11:23:29 +0900

    add: searchBookDelegate  delegate 추가

--- BookReviewApp/Scene/SearchBook/SearchBookViewController.swift
+++ BookReviewApp/Scene/SearchBook/SearchBookViewController.swift
@@ -10,7 +10,9 @@ import SnapKit
 
 final class SearchBookViewController: UIViewController {
     
-    private lazy var presenter = SearchBookPresenter(viewController: self)
+    private lazy var presenter = SearchBookPresenter(viewController: self, delegate: searchBookDelegate)
+    
+    private let searchBookDelegate: SearchBookDelegate
     
     private lazy var tableView: UITableView = {
         let tableView = UITableView()
@@ -18,6 +20,16 @@ final class SearchBookViewController: UIViewController {
         tableView.dataSource = presenter
         return tableView
     }()
+    
+    init(searchBookDelegate: SearchBookDelegate) {
+        self.searchBookDelegate = searchBookDelegate
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,7 +59,12 @@ extension SearchBookViewController : SearchBookProtocol {
     }
     
     func dismiss() {
+        navigationItem.searchController?.dismiss(animated: true)
         dismiss(animated: true)
+    }
+    
+    func reloadView() {
+        tableView.reloadData()
     }
 
 }
