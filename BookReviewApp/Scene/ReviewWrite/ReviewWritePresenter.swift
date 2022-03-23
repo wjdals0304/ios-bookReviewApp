f388e5e4cdd8b0deefef864de35906fe1cfd8635
jmkim <wjdals0304@gmail.com>
2022-03-23 11:20:03 +0900

    add:  right button  액션 추가

--- BookReviewApp/Scene/ReviewWrite/ReviewWritePresenter.swift
+++ BookReviewApp/Scene/ReviewWrite/ReviewWritePresenter.swift
@@ -12,11 +12,17 @@ protocol ReviewWriteProtocol {
     func showCloseAlertController()
     func close()
     func setupViews()
+    func presentToSearchBookViewController()
+    func updateViews(title: String, imageURL: URL?)
 }
 
 final class ReviewWritePresenter{
     private let viewController: ReviewWriteProtocol
+    private let userDefaultManager = UserDefaultManager()
 
+    private var book: Book?
+    let contentsTextViewPlaceHolderText = "내용을 입력해주세요"
+    
     init(viewController: ReviewWriteProtocol) {
         self.viewController = viewController
     }
@@ -29,7 +35,26 @@ final class ReviewWritePresenter{
         viewController.showCloseAlertController()
     }
     
-    func didTapRightBarButton() {
+    func didTapRightBarButton(contentsText: String?) {
+        
+        guard let book = book , let contentsText = contentsText,
+        contentsText != contentsTextViewPlaceHolderText 
+        else { return }
+        
+        let bookReview = BookReview(title: book.title, contents: contentsText, imageURL: book.imageURL)
+        userDefaultManager.setReview(bookReview)
         viewController.close()
+    }
+    
+    func didTapBookTitleButton() {
+        viewController.presentToSearchBookViewController()
+    }
+    
+}
+
+extension ReviewWritePresenter : SearchBookDelegate {
+    func selectBook(_ book: Book) {
+        self.book = book
+        viewController.updateViews(title: book.title, imageURL: book.imageURL)
     }
 }
